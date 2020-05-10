d556adc4202fad0db1ceebe0e449f740bb5b4a0f
Caroline Hoeg <carolinehoeg123@gmail.com>
2020-05-10 20:06:46 +0200

    deleted old code from home

--- src/Home.js
+++ src/Home.js
@@ -3,12 +3,6 @@ import React from "react";
 export function Home() {
 	return (
 		<div>
-				<b>Change them?</b> To login as user or admin, use the following
-				usernames and password:
-			<p>Username: admin, password: test</p>
-			<p>Username: user1, password: test</p>
-			<p>Username: user2, password: test</p>
-			<p>Username: user3, password: test</p>
 		</div>
 	);
 }
